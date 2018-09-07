8466a23b0cb040d54bb7192261f239a35b8fe0d8
chaitany10 <chaitany.pandiya@gmail.com>
2018-09-07 22:39:15 +0530

    Added previous codes

---
+++ 980C.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> v;
+    int n,k;
+    cin>>n>>k;
+
+}
---
+++ 990A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define m 1000000007
+int main()
+{
+
+    long long n,m,a,b;
+    cin>>n>>m>>a>>b;
+    if(n==m)
+        cout<<0;
+    else if(n>m)
+    {
+
+        if(n%m==0)
+            cout<<0;
+        else
+        {
+
+            long long op1=
+        }
+    }
+
+}
